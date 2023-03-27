89f84292b1ebb1599447695606d5d1c2f48ebdf1
suareze <suareze@predictia.es>
2023-03-27 09:14:39 +0200

    job to push metrics to Prometheus

--- cads_broker/metrics.py
+++ cads_broker/metrics.py
@@ -1,5 +1,29 @@
-import prometheus_client
+import os
+from urllib.error import URLError
 
-GENERATED_BYTES_COUNTER = prometheus_client.Counter(
+import structlog
+from prometheus_client import REGISTRY, Counter, push_to_gateway
+
+from cads_broker import config
+
+config.configure_logger()
+logger: structlog.stdlib.BoundLogger = structlog.get_logger(__name__)
+
+
+GENERATED_BYTES_COUNTER = Counter(
     "generated_bytes_counter", "Total bytes generated in successful requests"
 )
+
+exporter_host = os.environ.get("EXPORTER_HOST")
+
+
+def push_to_prometheus():
+    if exporter_host is not None:
+        try:
+            push_to_gateway(
+                exporter_host, job="cads-broker-metrics-job", registry=REGISTRY
+            )
+        except URLError:
+            logger.warning("Connection to Prometheus host refused.")
+        except Exception:
+            logger.error("Unexpected error")
