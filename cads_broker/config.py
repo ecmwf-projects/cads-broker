e64420ac48ceb3c9fd055ed273779e20a7779162
Concetta <c.tosto@bopen.eu>
2023-04-17 15:03:33 +0200

    Edit logging

--- cads_broker/config.py
+++ cads_broker/config.py
@@ -21,6 +21,7 @@ import pydantic
 import structlog
 
 from cads_broker import expressions
+import cads_common.logging
 
 dbsettings = None
 
@@ -100,24 +101,5 @@ def configure_logger() -> None:
 
     This function configures the logging module to log in rfc5424 format.
     """
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(message)s",
-        stream=sys.stdout,
-    )
-
-    structlog.configure(
-        processors=[
-            structlog.contextvars.merge_contextvars,
-            structlog.stdlib.filter_by_level,
-            structlog.stdlib.add_logger_name,
-            structlog.stdlib.add_log_level,
-            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
-            structlog.processors.StackInfoRenderer(),
-            structlog.processors.format_exc_info,
-            structlog.processors.JSONRenderer(),
-        ],
-        wrapper_class=structlog.stdlib.BoundLogger,
-        logger_factory=structlog.stdlib.LoggerFactory(),
-        cache_logger_on_first_use=True,
-    )
+    cads_common.logging.structlog_configure()
+    cads_common.logging.logging_configure()
