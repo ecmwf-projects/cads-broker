5691381c357b85523e4b4168f9eb60f0fb758e73
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2023-05-22 17:45:36 +0200

    bugfix: managed case of db incomplete but existing

--- cads_broker/database.py
+++ cads_broker/database.py
@@ -427,7 +427,15 @@ def init_database(connection_string: str, force: bool = False) -> sa.engine.Engi
         BaseModel.metadata.drop_all(engine)
         BaseModel.metadata.create_all(engine)
         alembic.command.stamp(alembic_cfg, "head")
-    elif force:
+    else:
+        # check the structure is empty or incomplete
+        query = sa.text(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
+        )
+        conn = engine.connect()
+        if "system_requests" not in conn.execute(query).scalars().all():
+            force = True
+    if force:
         # cleanup and create the schema
         BaseModel.metadata.drop_all(engine)
         BaseModel.metadata.create_all(engine)
