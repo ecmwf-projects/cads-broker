790d9b96859dfa9d0625c7d7f80307a9d77b05ff
Francesco Nazzaro <francesconazzaro89@gmail.com>
2023-06-14 14:10:08 +0200

    add alembic migration

---
+++ alembic/versions/cc6cc1cb3529_add_portal_field.py
@@ -0,0 +1,25 @@
+"""Add portal field
+
+Revision ID: cc6cc1cb3529
+Revises: d51c4dd28c1b
+Create Date: 2023-06-14 14:00:25.914273
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cc6cc1cb3529'
+down_revision = 'd51c4dd28c1b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column("system_requests", sa.Column("portal", sa.Text))
+    op.execute("UPDATE system_requests SET portal='c3s'")
+
+
+def downgrade() -> None:
+    op.drop_column("system_requests", "portal")
