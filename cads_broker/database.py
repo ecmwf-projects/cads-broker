a57b09705e89c8974f1ee0f031d20dd747199eba
Francesco Nazzaro <francesconazzaro89@gmail.com>
2023-10-12 14:53:20 +0200

    logs

--- cads_broker/database.py
+++ cads_broker/database.py
@@ -356,7 +356,7 @@ def requeue_request(
         request.request_metadata = metadata
         request.status = "accepted"
         session.commit()
-        logger.info("requeueing request", **logger_kwargs(request=request))
+        logger.info("--------- requeueing request", **logger_kwargs(request=request))
         return request
     else:
         return
