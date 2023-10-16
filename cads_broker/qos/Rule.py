c823f4844f374b958d57c1f1972b1a3340800e3e
Francesco Nazzaro <francesconazzaro89@gmail.com>
2023-10-16 12:42:55 +0200

    fix duplicate qos_status

--- cads_broker/qos/Rule.py
+++ cads_broker/qos/Rule.py
@@ -6,10 +6,6 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 #
-import structlog
-
-logger: structlog.stdlib.BoundLogger = structlog.get_logger(__name__)
-
 class Context:
     def __init__(self, request, environment):
         self.request = request
