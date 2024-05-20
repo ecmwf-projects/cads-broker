a6191fe32e0af14531df85f970fa0836d7d23f93
Francesco Nazzaro <francesconazzaro89@gmail.com>
2024-05-20 18:51:39 +0200

    rename requeue env var

--- cads_broker/dispatcher.py
+++ cads_broker/dispatcher.py
@@ -269,7 +269,7 @@ class Broker:
             else:
                 # FIXME: check if request status has changed
                 if os.getenv(
-                    "BROKER_REQUEUE_ON_KILLED_WORKER", False
+                    "BROKER_REQUEUE_ON_LOST_REQUESTS", False
                 ) and request.request_metadata.get("resubmit", 0) < os.getenv(
                     "BROKER_REQUEUE_LIMIT", 3
                 ):
@@ -328,7 +328,7 @@ class Broker:
                 error_message = "".join(traceback.format_exception(exception))
                 error_reason = exception.__class__.__name__
                 request = db.get_request(future.key, session=session)
-                requeue = os.getenv("BROKER_REQUEUE_ON_KILLED_WORKER", False)
+                requeue = os.getenv("BROKER_REQUEUE_ON_LOST_REQUESTS", False)
                 if error_reason == "KilledWorker":
                     worker_restart_events = self.client.get_events(
                         "worker-restart-memory"
