7e2e04cc032563b20ef8c1fb5b2c387c8379c80d
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2024-06-13 15:03:44 +0200

    more indexes on system_requests (#114)
    * more indexes on system_requests
    
    * style
    
    * down_revision right now
    
    * styl

---
+++ alembic/versions/d5d4afc97d40_more_indexes_on_system_requests.py
@@ -0,0 +1,31 @@
+"""more indexes on system requests.
+
+Revision ID: d5d4afc97d40
+Revises: ca178571bdc5
+Create Date: 2024-06-13 12:36:47.252394
+
+"""
+
+import alembic
+
+# revision identifiers, used by Alembic.
+revision = "d5d4afc97d40"
+down_revision = "ca178571bdc5"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    alembic.op.create_index("idx_system_requests_status", "system_requests", ["status"])
+    alembic.op.create_index(
+        "idx_system_requests_created_at", "system_requests", ["created_at"]
+    )
+    alembic.op.create_index(
+        "idx_system_requests_finished_at", "system_requests", ["finished_at"]
+    )
+
+
+def downgrade() -> None:
+    alembic.op.drop_index("idx_system_requests_status")
+    alembic.op.drop_index("idx_system_requests_created_at")
+    alembic.op.drop_index("idx_system_requests_finished_at")
