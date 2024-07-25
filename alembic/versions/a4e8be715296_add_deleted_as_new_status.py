93901a4db6aab76c3ce36440e8c9fa02a5201a64
Francesco Nazzaro <francesconazzaro89@gmail.com>
2024-07-25 18:35:06 +0200

    fix

--- alembic/versions/a4e8be715296_add_deleted_as_new_status.py
+++ alembic/versions/a4e8be715296_add_deleted_as_new_status.py
@@ -5,6 +5,7 @@ Revises: d5d4afc97d40
 Create Date: 2024-07-25 13:13:11.955119
 
 """
+import sqlalchemy as sa
 
 from sqlalchemy.dialects.postgresql import ENUM
 
@@ -17,10 +18,16 @@ branch_labels = None
 depends_on = None
 
 
-old_status_enum = ENUM(
-    "pending", "in_progress", "completed", "failed", name="status", create_type=False
+old_status_enum = sa.Enum(
+    "accepted",
+    "running",
+    "failed",
+    "successful",
+    "dismissed",
+    name="status",
+    create_type=False,
 )
-new_status_enum = ENUM(
+new_status_enum = sa.Enum(
     "pending",
     "in_progress",
     "completed",
