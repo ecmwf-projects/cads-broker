6d5d9608784ac1127e98f2fce13992f1f21fb319
Francesco Nazzaro <francesconazzaro89@gmail.com>
2024-11-15 11:49:53 +0100

    qa

--- cads_broker/dispatcher.py
+++ cads_broker/dispatcher.py
@@ -683,7 +683,9 @@ class Broker:
                     request, session=session_write, scheduler=self.internal_scheduler
                 )
                 if can_run and may_run and requests_counter < number_of_requests:
-                    self.submit_request(request, priority=user_cost, session=session_write)
+                    self.submit_request(
+                        request, priority=user_cost, session=session_write
+                    )
                     may_run = False
                     requests_counter += 1
 
@@ -716,7 +718,10 @@ class Broker:
                     requests_counter += 1
 
     def submit_request(
-        self, request: db.SystemRequest, session: sa.orm.Session, priority: int | None = None
+        self,
+        request: db.SystemRequest,
+        session: sa.orm.Session,
+        priority: int | None = None,
     ) -> None:
         """Submit the request to the dask scheduler and update the qos rules accordingly."""
         request = db.set_request_status(
