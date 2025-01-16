af93d20a1ab7732bb611b8c7f7bef6fccf39f903
Francesco Nazzaro <francesconazzaro89@gmail.com>
2025-01-16 14:31:45 +0100

    killed job

--- cads_broker/dispatcher.py
+++ cads_broker/dispatcher.py
@@ -432,11 +432,11 @@ class Broker:
         for request in dismissed_requests:
             if future := self.futures.pop(request.request_uid, None):
                 future.cancel()
-                kill_job_on_worker(self.client, request.request_uid)
             else:
                 # if the request is not in the futures, it means that the request has been lost by the broker
                 # try to cancel the job directly on the scheduler
                 cancel_jobs_on_scheduler(self.client, job_ids=[request.request_uid])
+            kill_job_on_worker(self.client, request.request_uid)
             session = self.manage_dismissed_request(request, session)
         session.commit()
 
