971e0cf981f8b1f6a84f996f488b75df95324402
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2025-03-14 16:59:29 +0100

    custom alembic cli for upgrade/downgrade

--- cads_broker/database.py
+++ cads_broker/database.py
@@ -19,7 +19,7 @@ from typing_extensions import Iterable
 
 import alembic.command
 import alembic.config
-from cads_broker import config
+from cads_broker import alembic_cli, config
 
 BaseModel = sa.orm.declarative_base()
 
@@ -932,15 +932,9 @@ def init_database(connection_string: str, force: bool = False) -> sa.engine.Engi
     :param force: if True, drop the database structure and build again from scratch
     """
     engine = sa.create_engine(connection_string)
-    migration_directory = os.path.abspath(os.path.join(__file__, "..", ".."))
-    os.chdir(migration_directory)
-    alembic_config_path = os.path.join(migration_directory, "alembic.ini")
-    alembic_cfg = alembic.config.Config(alembic_config_path)
-    for option in ["drivername", "username", "password", "host", "port", "database"]:
-        value = getattr(engine.url, option)
-        if value is None:
-            value = ""
-        alembic_cfg.set_main_option(option, str(value))
+    alembic_cfg = alembic.config.Config(alembic_cli.alembic_ini_path)
+    # passwords with special chars are urlencoded, but '%' must be escaped in ini files
+    alembic_cfg.set_main_option("sqlalchemy.url", connection_string.replace("%", "%%"))
     if not sqlalchemy_utils.database_exists(engine.url):
         sqlalchemy_utils.create_database(engine.url)
         # cleanup and create the schema
