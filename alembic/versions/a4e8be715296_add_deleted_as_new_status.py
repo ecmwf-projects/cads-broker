7e4b67e89ade8d8c3630ad9399bca9f195c359d3
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2025-03-18 09:59:38 +0100

    fixed some old migrations

--- alembic/versions/a4e8be715296_add_deleted_as_new_status.py
+++ alembic/versions/a4e8be715296_add_deleted_as_new_status.py
@@ -46,4 +46,9 @@ def upgrade() -> None:
 def downgrade() -> None:
     # Remove the new status from the enum
     # this doesn't work
-    op.execute("ALTER TYPE status DELETE VALUE 'deleted'")
+    #op.execute("ALTER TYPE status DELETE VALUE 'deleted'")
+    op.execute("CREATE TYPE status_old AS ENUM ('accepted','running','failed','successful','dismissed')")
+    op.execute("DELETE FROM system_requests where status='deleted'")
+    op.execute("ALTER TABLE system_requests ALTER COLUMN status TYPE status_old USING (status::text::status_old)")
+    op.execute("DROP TYPE status")
+    op.execute("ALTER TYPE status_old RENAME TO status")
