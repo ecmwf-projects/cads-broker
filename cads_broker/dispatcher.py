aae93c3ae7fca46119fda7dc7326171707d8d387
Francesco Nazzaro <francesconazzaro89@gmail.com>
2025-04-08 11:51:00 +0200

    add a comment

--- cads_broker/dispatcher.py
+++ cads_broker/dispatcher.py
@@ -862,6 +862,7 @@ class Broker:
         for scheduler in random.sample(list(self.schedulers.keys()), k=len(self.schedulers)):
             client = self.schedulers[scheduler]
             resources = request.request_metadata.get("resources", {})
+            # check if the resources are available on the workers pool
             if set(resources.keys()).issubset(get_workers_resources(client)):
                 request.request_metadata["scheduler"] = scheduler
                 future = client.submit(
